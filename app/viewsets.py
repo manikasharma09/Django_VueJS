03775ca0c299ea8ba5024046d115e77d84528141
Manika Sharma <51908554+manikasharma09@users.noreply.github.com>
2020-05-22 15:55:16 +0530

    Add files via upload

---
+++ app/viewsets.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .models import *
+from .serializers import *
+
+class ArticleViewSet(viewsets.ModelViewSet):
+	queryset=Article.objects.all()
+	serializer_class=ArticleSerializer
